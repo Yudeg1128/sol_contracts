02b9fb306100d923f831b0aa77416ee14b9263cc
Yudeg1128 <munkhbat1128@gmail.com>
2020-08-19 18:07:34 +0800

    First Commit

---
+++ stablecoin.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.6.8;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+
+contract MidasCoin is ERC20, Ownable {
+
+    constructor() ERC20("MidasStableCoin", "MSC") public {
+        _setupDecimals(0);
+    }
+
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burn(address from, uint256 amount) external onlyOwner {
+        _burn(from, amount);
+    }
+}
