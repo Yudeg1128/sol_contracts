bfc4a6ecdfd19a2abc20057e26569590e6a3934f
Yudeg1128 <67855177+Yudeg1128@users.noreply.github.com>
2021-08-04 23:04:33 -0700

    Create stablecoin08.sol

---
+++ stablecoin08.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+
+contract AICoin is ERC20, Ownable {
+
+    constructor() ERC20("AICoin", "AIC") {}
+
+    function decimals() public view virtual override returns (uint8) {
+        return 0;
+    }
+    
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burn(address from, uint256 amount) external onlyOwner {
+        _burn(from, amount);
+    }
+}
